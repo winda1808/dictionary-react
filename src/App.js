bdfcec347740e4b781882a4681840640b9eeb348
winda1808 <105516942+winda1808@users.noreply.github.com>
2022-07-30 16:22:08 +0800

    formatting form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
-import hello from "./hello.png"
+import hello from "./hello.png";
+import Dictionary from './Dictionary';
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-        <img src={hello} alt="placeholder" className="img-fluid"/>
+        <img src={hello} alt="placeholder" className="Image img-fluid"/>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="footer">
+        open source code by Winda Andhini
+      </footer>
+    </div>
     </div>
   );
 }
